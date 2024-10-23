315e44701fd83eae29e8044e4b239973ac1b1065
David Alonso <125155276+DavidAlonsoRod@users.noreply.github.com>
2024-10-23 18:36:07 +0000

    Excercise

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
+const {sum} = require ('./app.js')
+
 test("One euro should be 1.07 dollars", function() {
     // Importo la funcion desde app.js
     const { fromEuroToDollar } = require('./app.js');
@@ -10,4 +12,27 @@ test("One euro should be 1.07 dollars", function() {
 
     // Hago mi comparación (la prueba)
     expect(fromEuroToDollar(3.5)).toBe(3.745); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+test("One dollar should be 147,71 Yens", function() {
+    // Importo la funcion desde app.js
+    const { fromDollarToYen } = require('./app.js');
+
+    // Uso la función como debe ser usada
+    const yen = fromDollarToYen(3.5);
+
+    
+    const expected = 3.5 * 147.71;
+
+    
+    expect(fromDollarToYen(3.5)).toBe(511.91588785046724); // 1 euro son 1.07 dólares, entonces 3.5 euros deberían ser = (3.5 * 1.07)
+})
+test("One Yen should be 0.0053 Pounds", function() {
+
+    const { fromYenToPound } = require('./app.js');
+
+    const Pounds = fromYenToPound(3.5);
+
+    const expected = 3.5 * 0.0053;
+
+    expect(fromYenToPound(3.5)).toBe(0.019456869009584665); 
 })
